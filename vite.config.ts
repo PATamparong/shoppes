5e441fd8e1d319eb17b9f8a85a6fbfabb835e948
Precious <preciousangel.tamparong@student.passerellesnumeriques.org>
2023-10-25 17:42:45 +0800

    update base

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/shoppes/",
   //automatic imports
   // esbuild: {
   //   jsxInject: `import React from 'react'`,
